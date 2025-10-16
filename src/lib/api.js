2303f19b9c705c605d95cf357eca851781fe6216
zydovvn <assadin07@gmail.com>
2025-10-16 14:25:48 +0700

    fix

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-export const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API =
+  import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API,
   withCredentials: true,
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
+export { api, API };
